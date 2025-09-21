64ab83de22dfaf062aaea52e5ce8ab499870ccef
Ettore Rossatto <ettore.rossatto@osapiens.com>
2025-09-22 01:09:35 +0200

    feat: implement municipality repository

--- src/infrastructure-layer/db-sqlite-sequelize/repositories/municipality.sequelize-repository.ts
+++ src/infrastructure-layer/db-sqlite-sequelize/repositories/municipality.sequelize-repository.ts
@@ -1,6 +1,7 @@
 import { IMunicipalityRepository } from "../../../domain-layer/repositories/municipality.domain-interface-repository";
 import { Municipality } from "../../../domain-layer/entities/municipality.entity";
 import { MunicipalityId } from "../../../domain-layer/identifiers/municipality-id.identifier";
+import { MunicipalityName } from "../../../domain-layer/value-objects/municipality-name.value-object";
 import { Municipality as MunicipalityModel } from "../models/municipality.sequelize-model";
 import { MunicipalityMapper } from "../mappers/municipality.infrastructure-mapper";
 
@@ -12,7 +13,15 @@ export class MunicipalitySequelizeRepository
     return row ? MunicipalityMapper.toDomain(row) : null;
   }
 
+  async findByName(name: MunicipalityName): Promise<Municipality | null> {
+    const row = await MunicipalityModel.findOne({
+      where: { name: name.value },
+    });
+    return row ? MunicipalityMapper.toDomain(row) : null;
+  }
+
   async save(entity: Municipality): Promise<void> {
+    // TODO: what if is there already a municipality with the same name?
     await MunicipalityModel.upsert({
       id: entity.id.value,
       name: entity.name.value,
